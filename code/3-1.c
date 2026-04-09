ebb130d88b786e9556d42c5319e79aae89b518e9
Jeongeun Baek <127070509+bjo5029@users.noreply.github.com>
2026-04-09 22:15:36 +0900

    Update 3-1.c

--- code/3-1.c
+++ code/3-1.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 
-void read_only(int const *arr, int size) {
-
-    arr[0] = 999;
-
+void read_only(const int *arr, int size) {
+    // arr[0] = 999; 
     for (int i = 0; i < size; i++) {
         printf("%d\n", *(arr + i));
     }
- }
+}
 
- int main() {
-    int num[] = {10, 20, 30, 40, 50};
-    read_only(num, 5);
+int main() {
+    int nums[] = {10, 20, 30, 40, 50};
+    read_only(nums, 5);
     return 0;
- }
+}
